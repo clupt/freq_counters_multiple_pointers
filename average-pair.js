5567a220dec02ca9c493b7c43e275c7907e3c0ca
Chris Lupton <42554017+clupt@users.noreply.github.com>
2023-02-24 18:05:17 -0600

    working average pair and construct word and added jasmine test.html for average pair test

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,43 @@
-// add whatever parameters you deem necessary & write docstring
-function averagePair() {
+/**
+ * This function calculates if there is a pair of numbers within an array
+ * that can make the target average
+ *
+ * it takes in a sorted array of numbers and a target average
+ *
+ * it returns a true if it can and false it can't such find a pair
+ */
+function averagePair(nums, targetAvg) {
+  console.log("averagePair=", averagePair, "nums=", nums, "targetAvg=", targetAvg);
+  //check to see if nums is empty (edge case)
+  if (!nums.length) return false;
+  //make a left and right pointer
+  let left = 0;
+  let right = nums.length - 1;
+  //loop while the left pointer is less than the right pointer
+  while (left < right) {
+    //store the average
+    let average = (nums[left] + nums[right]) / 2;
+    console.log("average=", average);
+    console.log("targetAverage=", targetAvg);
+    console.log("left=", left);
+    console.log("right=", right);
+    console.assert(([1, 2, 3], 2.5) === true);
+    //if their average is the target average return true
+    if (average === targetAvg) {
+      return true;
+    }
+    //if the average is too small, move the left pointer
+    else if (average < targetAvg) {
+      left++;
+    }
+    //if the average it too large, move the right pointer
+    else {
+      right--;
+    }
+  }
+  //outside of the loop, return false
+  return false;
 }
+
+averagePair()
 
--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,60 @@
-// add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+"use strict";
+
+/** this function will serve as our frequency counter for the problems
+ *
+ * it will take in a an iterable
+ *
+ * it will return a hashmap of the frequencies of the values at the indices in
+ * the iterable
+ */
+
+function frequencyCounter(iterable) {
+  console.log("frequencyCounter=", frequencyCounter, "iterable=", iterable);
+  const freqMap = {};
+  //populate the map with the counts of the values in the iterable
+  for (let value of iterable) {
+    freqMap[value] = (freqMap[value] || 0) + 1;
+  }
+  console.log("freqMap=", freqMap);
+  return freqMap;
+}
+
+
+
+/** this function will determine if a word input can be constructed by a letters
+ * input
+ *
+ * it was take two strings - one a word and one letters
+ *
+ * it will return a boolean --whether or not the all of the letters are found
+ * in the word the appropriate number of times
+ *
+ * */
+
+function canConstructWord(word, letters) {
+  console.log("canConstructWord", "word=", word, "letters=", letters);
+  //short circuit to false if word is longer than letters
+  if (word.length < letters.length) return false;
+
+  //make a frequency counter for each two input
+  const wordMap = frequencyCounter(word);
+  console.log("wordMap=", wordMap);
+
+  const lettersMap = frequencyCounter(letters);
+  console.log("lettersMap=", lettersMap);
+
+  //loop through the word counter and confirm that there are enough vals
+  //at that key in the letters counter
+  //short circuit if the key isnt in the letters counter at all
+  for (let key in wordMap) {
+    if (!(key in lettersMap)) {
+      console.log("key=", key);
+      return false;
+    }
+    else if (wordMap[key] < lettersMap[key]) {
+      return false;
+    }
+  }
+  //return true
+  return true;
 }

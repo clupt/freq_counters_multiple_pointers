9d100ff275d4772eed215d83615c0698b2ca3efe
William Squires <william.squiresii@gmail.com>
2023-02-24 18:45:05 -0600

    added use strict to all files and finished two-array-object

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,27 @@
-// add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+"use strict";
+/**
+ * takeArrayObjects takes 2 arrays, one of keys and one of values
+ * 
+ * it returns an object created from those keys and values
+ * 
+ * excess keys have their value set to null
+ * excess values are ignored
+ */
+function twoArrayObject(keys, values) {
+    console.log(`twoArrayObject ${keys} ${values}`);
+    const obj = {};
+    //loop over keys
+    for (let i in keys) {
+        // set key value pair if we still have values
+        if (i < values.length) {
+            obj[keys[i]] = values[i];
+        }
+        // otherwise, set value to null
+        else {
+            obj[keys[i]] = null;
+        }
+    }
+    console.log(obj);
+    //return our object
+    return obj;
 }
